2236c1f1f757827ea580de761285421bd90e0469
mikmk <weped_x@163.com>
2024-06-12 22:25:01 +0800

    layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,14 @@
-function App(): JSX.Element {
+import { Content, DraggableTopBar, RootLayout, Sidebar } from './components'
+
+const App = (): JSX.Element => {
   return (
-    <div className="flex h-full items-center justify-center">
-      <span className="text-4xl text-blue-500">APP</span>
-    </div>
+    <>
+      <DraggableTopBar />
+      <RootLayout>
+        <Sidebar className="p-2">Sidebar</Sidebar>
+        <Content className="border-1 bg-zinc-300/50 border-l-black/50">Content</Content>
+      </RootLayout>
+    </>
   )
 }
 
